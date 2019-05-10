aea6da8c86bc779c9e6207b2707bcfebd6ee2dee
Avekatten <Andreaslyksen@gmail.com>
2019-05-10 17:00:08 +0200

    Created a login method and added some stuff

--- APICollection/Controllers/UserController.cs
+++ APICollection/Controllers/UserController.cs
@@ -1,24 +1,27 @@
 ﻿using APICollection.Helpers;
 using APICollection.Models;
 using Raven.Client.Documents.Session;
-using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Runtime.Serialization.Json;
-using System.Text;
 using System.Web.Http;
 
 namespace APICollection.Controllers
 {
+    [RoutePrefix("api/User")]
     public class UserController : ApiController
     {
         // GET: api/User
         public User Get()
         {
-            return new User();
+            using (IDocumentSession session = RavenDocumentStore.Store.OpenSession())  // Open a session for a default 'Database'
+            {
+                User user = session.Load<User>("1");                               // Load the Product and start tracking
+
+
+
+                session.SaveChanges();
+                return user;
+            }
         }
 
         // GET: api/User/5
@@ -50,13 +53,49 @@ namespace APICollection.Controllers
         }
 
         // PUT: api/User/5
-        public void Put(int id, [FromBody]string value)
+        public void Put(int id, [FromBody]User value)
         {
+            using (IDocumentSession session = RavenDocumentStore.Store.OpenSession())  // Open a session for a default 'Database'
+            {
+                User user = session.Load<User>("users/" + id + "-A");                               // Load the Product and start tracking
+
+                session.Delete(user);
+
+                session.SaveChanges();
+            }
         }
 
         // DELETE: api/User/5
         public void Delete(int id)
         {
+            using (IDocumentSession session = RavenDocumentStore.Store.OpenSession())  // Open a session for a default 'Database'
+            {
+                User user = session.Load<User>("users/" + id + "-A");                               // Load the Product and start tracking
+
+                session.Delete(user);
+
+                session.SaveChanges();
+            }
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public bool Login([FromBody]User user)
+        {
+            using (IDocumentSession session = RavenDocumentStore.Store.OpenSession())
+            {
+                List<User> matchingUser = session
+                    .Query<User>()                               
+                    .Where(x => x.Username.Equals(user.Username) && x.Password.Equals(user.Password))
+                    .ToList();
+
+                if (matchingUser.Count > 0)
+                {
+                    return true;
+                }
+
+                return false;
+            }
         }
     }
 }
